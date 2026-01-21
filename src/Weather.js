6f25c41bac683ab43ed252dbbbd373a3b5f1b5cb
lienta3 <lien.ta3@gmail.com>
2026-01-21 12:04:37 +1100

    amended code for page to render once city submitted and not by each keystroke in city

--- src/Weather.js
+++ src/Weather.js
@@ -19,10 +19,23 @@ export default function Weather() {
         icon: response.data.condition.icon_url,
       });
     });
-  }, [city]);
+  }, []);
 
   function handleClick(event) {
     event.preventDefault();
+
+    let api_key = "b2a5adcct04b33178913oc335f405433";
+    let api_url = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${api_key}&units=metric`;
+    axios.get(api_url).then((response) => {
+      setWeather({
+        cityResult: response.data.city,
+        temperature: response.data.temperature.current,
+        condition: response.data.condition.description,
+        humidity: response.data.temperature.humidity,
+        wind: response.data.wind.speed,
+        icon: response.data.condition.icon_url,
+      });
+    });
   }
   function updateCity(event) {
     setCity(event.target.value);
